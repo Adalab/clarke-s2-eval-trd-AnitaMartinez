da613abc9c94985a756f342043f6c61bc9dad2cb
“Ana” <amartinezaguilar6@gmail.com>
2017-12-02 19:35:47 +0100

    Compare number user with random number and show message

--- main.js
+++ main.js
@@ -2,8 +2,45 @@
 
 
 // Número aleatorio
+
 function getRandomInt(min, max) {
     return Math.ceil(Math.random() * (max - min)) + min;
 }
 
-console.log(getRandomInt(1,100));
+var numberRandom = getRandomInt(1,100);
+console.log(numberRandom); // Borrar
+
+
+
+
+// Comparar el número del usuario y el número aleotorio y lanzar mensajes
+
+var numberUserAsString = '50'; // Vendría por el input
+var numberUser = parseInt(numberUserAsString);
+
+var messageToUser = document.querySelector('#message-to-user');
+
+if (numberUser === numberRandom) {
+  messageToUser.innerHTML = ('¡Acertaste!');
+} else if (numberUser > numberRandom) {
+  messageToUser.innerHTML = 'Demasiado alto';
+} else {
+  messageToUser.innerHTML = 'Demasiado bajo';
+}
+
+
+
+
+
+
+/*
+var firstName = prompt('como te llamas?');
+if (firstName == 'Ana' || firstName =='Gemma') {
+    alert('Hola bellas');
+
+} else {
+  alert('fuera!')
+
+}
+
+*/
