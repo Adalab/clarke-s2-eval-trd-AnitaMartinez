e009dbb95576362f8e7023f8e5a9e31a0707d8fc
“Ana” <amartinezaguilar6@gmail.com>
2017-12-06 11:22:21 +0100

    Redo function removeClass to adapt it with events

--- main.js
+++ main.js
@@ -8,27 +8,24 @@ return Math.ceil(Math.random() * (max - min)) + min;
 var numberRandom = getRandomInt(1,100);
 console.log(numberRandom);
 
-// var numberRandom = 50;  -- Borrar
-
-
 
 //Cuando el usuario pulsa el botón de Prueba...
   // Guardo el número que introduce
   // Comparo el número del usuario y el número aleatorio y lanzo mensajes
+  // Actualizo el contador
+  //Si acierta, muestro el div con el botón de escribe tu nombre
 
 var tryButton = document.querySelector("#try-buttom");
 tryButton.addEventListener('click', ValueInput);
-
 
 function ValueInput() {
   var inputUserNumber = document.querySelector("#inputUserNumber");
   var valueinputUserNumberAsString = inputUserNumber.value;
   var valueinputUserNumber = parseInt(valueinputUserNumberAsString); //Guardo el value del input
   messagesGame('#message-to-user', valueinputUserNumber, numberRandom,'¡Acertaste!', 'Demasiado alto', 'Demasiado bajo'); //hago comparaciones y pinto mensajes
-  addAndPaint(); // actualizar el contador
-
-}
-
+  addAndPaint(); // actualizo el contador
+  removeClass('#container-input-name', valueinputUserNumber, numberRandom,'display-none'); //Si acierta, muesto la caja para guardar el nombre
+};
 
 //Función para comparaciones entre aleatorio y el número del usuario y pinto mensaje
 function messagesGame(selector, condition1, condition2, messageWin, messageHigh, messageLow){
@@ -51,37 +48,20 @@ function addAndPaint() {
   attemps.innerHTML = (acumulador);
 }
 
-
-/*
-
-// Cuando el jugador acierte el número, aparece la caja de introducir nombre y el botón
+//Función para que si el usuario acierta, aparezca la caja del escribe tu nombre
 
 function removeClass(selector, condition1, condition2, nameClass) {
   var elementSelector = document.querySelector(selector);
   if (condition1 === condition2) {
     elementSelector.classList.remove(nameClass);
-  }
+  };
 }
 
-removeClass('#container-input-name', numberUser, numberRandom,'display-none');
 
-*/
 
-// Actualizar el contador
+
 
 /*
-
-function saveAttemps() {
-
-
-
-
-
-  tryButton.addEventListener("click",function(){
-
-    var value = document.querySelector("#inputUserNumber").value;  // Aquí recojo el valor del input numberUser
-    console.log(value);
-
 
 
     //Guardar el número de intentos en la propiedad de un objeto)
